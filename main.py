71339dc7a466d9774526487c7ef36cfa641d1d4f
HISHAN03 <moidinhishan@gmail.com>
2024-07-14 16:38:28 +0530

    menu-update-v2-and-sound

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from grid.Grid1 import Grid1 as Grid1
-from grid.Grid2 import Grid2 as Grid2
 from grid.DefaultGrid import DefaultGrid as DefaultGrid
 from dijkstra import Dijkstra
 from button import Button
+import subprocess
+# glut_window_id = None 
 
 # Global variables
 window_width = 600
@@ -18,29 +19,32 @@ algorithm_started = False
 dijkstra_algorithm = None
 current_screen = "menu"
 
-
 base_color = "#b68f40"
 lighter_shade = "#d9b96b"
 darker_shade = "#8c7029"
 slightly_lighter_shade = "#c8a85a"
 slightly_darker_shade = "#a27e37"
 
+
+# Load sound effects
+pygame.mixer.init()
+start_sound = pygame.mixer.Sound("assets/drop.mp3")
+
+
+
 # Pygame initialization
 pygame.init()
 screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Dijkstra's Algorithm Visualization - Menu")
 font = pygame.font.SysFont('Arial', 24)
-clock = pygame.time.Clock()
 
-
-flicker_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-current_flicker_color_index = 0
-
+BG = pygame.image.load("assets/Background.png")
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
 
 def display_menu():
-    screen.fill((255, 255, 255))
+    screen.blit(BG,(0,0))
+    #screen.fill((255, 255, 255))
     # Title text
     title_surface1 = get_font(27).render("Dijkstra's Algorithm", True, "#b68f40")
     title_surface2 = get_font(25).render("Visualization", True, "#b68f40")
@@ -57,13 +61,11 @@ def display_menu():
     screen.blit(continue_surface, (window_width // 2 - continue_surface.get_width() // 2, window_height // 2 + 30))
 
     pygame.display.flip()
+    # start_sound.play()
 
-def update_flicker_color():
-    global current_flicker_color_index
-    current_flicker_color_index = (current_flicker_color_index + 1) % len(flicker_colors)
 
 def display_level_selection():
-    screen.fill((255, 255, 255))
+    screen.blit(BG,(0,0))
 
     title_surface = get_font(24).render("Select Grid :", True, "#b68f40")
     screen.blit(title_surface, (window_width // 2 - title_surface.get_width() // 2, window_height // 2 - 50))
@@ -75,6 +77,26 @@ def display_level_selection():
     screen.blit(grid2_surface, (window_width // 2 - grid2_surface.get_width() // 2, window_height // 2 + 30))
 
     pygame.display.flip()
+    
+    
+# def restart_program():
+#     global glut_window_id
+#     if glut_window_id is not None:
+#         glutDestroyWindow(glut_window_id)
+
+#     # Restart the Python script
+#     python = sys.executable
+#     subprocess.call([python, sys.argv[0]])
+
+
+# Define sound file path
+sound_file_path = "assets/drop.mp3"
+
+def init_sound():
+    # Load sound file
+    pygame.mixer.init()
+    global start_stop_sound
+    start_stop_sound = pygame.mixer.Sound(sound_file_path)
 
 
 def run_menu():
@@ -89,8 +111,7 @@ def run_menu():
                 current_screen = "level_selection"
 
         display_menu()
-        update_flicker_color()
-        clock.tick(1)
+
 
 def run_level_selector():
     global current_screen, grid
@@ -105,20 +126,20 @@ def run_level_selector():
                     grid = Grid1(grid_size)
                     current_screen = "game"
                 elif event.key == pygame.K_2:
-                    grid = Grid2(grid_size)
-                    current_screen = "game"
-                elif event.key == pygame.K_3:
                     grid = DefaultGrid(grid_size)
                     current_screen = "game"
 
         display_level_selection()
-        clock.tick(30)
+
 
 # OpenGL initialization
 def init_opengl():
+    global start_stop_sound
+    init_sound()
     glutInit()
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     glutInitWindowSize(window_width, window_height)
+    # glut_window_id = glutCreateWindow(b"Dijkstra's Algorithm Visualization - Interactive")
     glutCreateWindow(b"Dijkstra's Algorithm Visualization - Interactive")
     glutDisplayFunc(display)
     glutMouseFunc(mouse)
@@ -142,14 +163,16 @@ def mouse(button, state, x, y):
         grid_y = (window_height - y) * grid_size[1] // window_height
         if grid.start_point is None:
             grid.start_point = (grid_x, grid_y)
+            start_stop_sound.play()
         elif grid.goal_point is None:
             grid.goal_point = (grid_x, grid_y)
+            start_stop_sound.play()
         else:
             grid.set_obstacle(grid_x, grid_y)
         glutPostRedisplay()
 
 def keyboard(key, x, y):
-    global algorithm_started, dijkstra_algorithm
+    global algorithm_started, dijkstra_algorithm, current_screen
 
     if key == b'\x1b':  # Escape key
         sys.exit()
@@ -158,6 +181,13 @@ def keyboard(key, x, y):
         dijkstra_algorithm = Dijkstra(grid)
         dijkstra_algorithm.init(grid.start_point)
         step_dijkstra(0)
+        
+        
+        
+        
+    # if key == b'q':  
+    #     restart_program()
+        
     glutPostRedisplay()
 
 def step_dijkstra(value):
